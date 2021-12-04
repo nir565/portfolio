1a5348a179c26c3b349858691324f5eccc4f3e16
nir565 <bt011singh@gmail.com>
2021-12-04 19:39:56 +0000

    removed old stuff

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6_%asvp66#rne@3%%)8-x2gn^rmskthlberqu82ef1zot3ox05'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['niraj565.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 
 MEDIA_URL = '/media/'
 
